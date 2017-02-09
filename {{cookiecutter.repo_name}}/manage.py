a2f08abf1cfc64d2f10edbf9a3919c18b68cd82e
arkii <sqy6@163.com>
2017-02-09 18:52:00 +0800

    tidy

--- {{cookiecutter.repo_name}}/manage.py
+++ {{cookiecutter.repo_name}}/manage.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python
-
 import os
+from app import create_app, db
+from app.models import User, Role
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
-from flask.ext.script import Manager, Server
-from flask.ext.script.commands import ShowUrls, Clean
-from {{cookiecutter.repo_name}} import create_app
-from {{cookiecutter.repo_name}}.models import db, User
-
-# default to dev config because no one should use this in
-# production anyway
-env = os.environ.get('{{cookiecutter.repo_name|upper}}_ENV', 'dev')
-app = create_app('{{cookiecutter.repo_name}}.settings.%sConfig' % env.capitalize())
-
+app = create_app(os.getenv('FLASK_ENV') or 'default')
 manager = Manager(app)
-manager.add_command("server", Server())
-manager.add_command("show-urls", ShowUrls())
-manager.add_command("clean", Clean())
+migrate = Migrate(app, db)
 
 
-@manager.shell
 def make_shell_context():
-    """ Creates a python REPL with several default imports
-        in the context of the app
-    """
-
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def createdb():
-    """ Creates a database with all of the tables defined in
-        your SQLAlchemy models
-    """
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
-    db.create_all()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     manager.run()
